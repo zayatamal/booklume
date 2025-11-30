b91f35ccbc3f72f759e4b546268dd5d9525d3441
Amal Zayat <zayatamal@gmail.com>
2025-11-30 23:36:10 +0100

    Stores reviews for now

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 import json
+from pathlib import Path
 
 title = input("Book Title:")
 author = input("Author Name:")
-rating = input("Give your star rating between 1 to 5 ")
-review_text = input("Write your Review")
-review = {"title": title, "author": author, "rating": rating, "review": review_text}
+rating = input("Give your star rating between 1 to 5: ")
+review_text = input("Write your Review: ")
+review = {
+    "title": title,
+    "author": author,
+    "rating": rating,
+    "review": review_text,
+}
+reviews_path = Path("reviews.json")
+try:
+    with open(reviews_path, "r") as f:
+        reviews = json.load(f)
+except:
+    reviews = []
+
+reviews.append(review)
+
+with open("reviews.json", "w") as f:
+    json.dump(reviews, f, indent=4)
+print("✅ Review saved!")
